92575594f6e864673d24073e8cabdcac11956b0e
Lance <156000490+whimsicalpretzel@users.noreply.github.com>
2026-02-16 20:15:52 +0000

    initial tests

---
+++ BusShuttleProject/BusShuttle.Tests/FileSaverTests.cs
@@ -0,0 +1,22 @@
+﻿namespace BusShuttle.Tests;
+
+using BusShuttle;
+
+public class FileSaverTests
+{
+    FileSaver fileSaver;
+    string testFileName;
+
+    public FileSaverTests() {
+        testFileName = "test-doc.txt";
+        fileSaver = new FileSaver(testFileName);
+    }
+
+    [Fact]
+    public void Test_FileSaver_Append()
+    {
+        fileSaver.AppendLine("Hello, World!");
+        var contentFromFile = File.ReadAllText(testFileName);
+        Assert.Equal("Hello, World!"+Environment.NewLine,contentFromFile);
+    }
+}
